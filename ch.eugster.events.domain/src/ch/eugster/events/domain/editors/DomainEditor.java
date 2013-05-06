dd862cdd012f81a1c08408282f3a4d3a7d1252dc
Christian Eugster <christian.eugster@gmx.net>
2013-05-06 08:24:49 +0000

    DomainEditor: Ergänzt mit Angaben zur Domain (Organization, Address, etc.)

--- ch.eugster.events.domain/src/ch/eugster/events/domain/editors/DomainEditor.java
+++ ch.eugster.events.domain/src/ch/eugster/events/domain/editors/DomainEditor.java
@@ -39,19 +39,21 @@ public class DomainEditor extends AbstractEntityEditor<Domain>
 
 	private Text description;
 
-	@Override
-	protected void initialize()
-	{
-		EntityMediator.addListener(Domain.class, this);
-	}
+	private Text organization;
 
-	@Override
-	protected void createSections(ScrolledForm parent)
-	{
-		this.createSection(parent);
-	}
+	private Text address;
 
-	private void createSection(ScrolledForm parent)
+	private Text city;
+
+	private Text phone;
+
+	private Text fax;
+
+	private Text email;
+
+	private Text website;
+
+	private void createDomainSection(final ScrolledForm parent)
 	{
 		ColumnLayoutData layoutData = new ColumnLayoutData();
 		layoutData.widthHint = 200;
@@ -62,19 +64,55 @@ public class DomainEditor extends AbstractEntityEditor<Domain>
 				| ExpandableComposite.COMPACT | ExpandableComposite.TITLE_BAR | ExpandableComposite.TWISTIE);
 		section.setLayoutData(layoutData);
 		section.setLayout(sectionLayout);
-		section.setText("Beschreibung");
-		section.setClient(this.fillSection(section));
+		section.setText("Domäne");
+		section.setClient(this.fillDomainSection(section));
 		section.addExpansionListener(new ExpansionAdapter()
 		{
 			@Override
-			public void expansionStateChanged(ExpansionEvent e)
+			public void expansionStateChanged(final ExpansionEvent e)
 			{
 				DomainEditor.this.scrolledForm.reflow(true);
 			}
 		});
 	}
 
-	private Control fillSection(Section parent)
+	private void createOrganizationSection(final ScrolledForm parent)
+	{
+		ColumnLayoutData layoutData = new ColumnLayoutData();
+		layoutData.widthHint = 200;
+		TableWrapLayout sectionLayout = new TableWrapLayout();
+		sectionLayout.numColumns = 1;
+
+		Section section = this.formToolkit.createSection(this.scrolledForm.getBody(), ExpandableComposite.EXPANDED
+				| ExpandableComposite.COMPACT | ExpandableComposite.TITLE_BAR | ExpandableComposite.TWISTIE);
+		section.setLayoutData(layoutData);
+		section.setLayout(sectionLayout);
+		section.setText("Organisation");
+		section.setClient(this.fillOrganizationSection(section));
+		section.addExpansionListener(new ExpansionAdapter()
+		{
+			@Override
+			public void expansionStateChanged(final ExpansionEvent e)
+			{
+				DomainEditor.this.scrolledForm.reflow(true);
+			}
+		});
+	}
+
+	@Override
+	protected void createSections(final ScrolledForm parent)
+	{
+		this.createDomainSection(parent);
+		this.createOrganizationSection(parent);
+	}
+
+	@Override
+	public void dispose()
+	{
+		EntityMediator.removeListener(Domain.class, this);
+	}
+
+	private Control fillDomainSection(final Section parent)
 	{
 		GridLayout layout = new GridLayout(2, false);
 
@@ -89,7 +127,8 @@ public class DomainEditor extends AbstractEntityEditor<Domain>
 		this.code.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
 		this.code.addModifyListener(new ModifyListener()
 		{
-			public void modifyText(ModifyEvent e)
+			@Override
+			public void modifyText(final ModifyEvent e)
 			{
 				if (DomainEditor.this.name.getText().isEmpty()
 						|| DomainEditor.this.name.getText().equals(
@@ -107,7 +146,8 @@ public class DomainEditor extends AbstractEntityEditor<Domain>
 		this.name.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
 		this.name.addModifyListener(new ModifyListener()
 		{
-			public void modifyText(ModifyEvent e)
+			@Override
+			public void modifyText(final ModifyEvent e)
 			{
 				DomainEditor.this.setDirty(true);
 			}
@@ -120,7 +160,8 @@ public class DomainEditor extends AbstractEntityEditor<Domain>
 		this.description.setLayoutData(new GridData(GridData.FILL_BOTH));
 		this.description.addModifyListener(new ModifyListener()
 		{
-			public void modifyText(ModifyEvent e)
+			@Override
+			public void modifyText(final ModifyEvent e)
 			{
 				DomainEditor.this.setDirty(true);
 			}
@@ -131,8 +172,132 @@ public class DomainEditor extends AbstractEntityEditor<Domain>
 		return composite;
 	}
 
+	private Control fillOrganizationSection(final Section parent)
+	{
+		GridLayout layout = new GridLayout(2, false);
+
+		Composite composite = this.formToolkit.createComposite(parent);
+		composite.setLayoutData(new TableWrapData(TableWrapData.FILL_GRAB));
+		composite.setLayout(layout);
+
+		Label label = this.formToolkit.createLabel(composite, "Name", SWT.NONE);
+		label.setLayoutData(new GridData());
+
+		this.organization = this.formToolkit.createText(composite, "");
+		this.organization.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+		this.organization.addModifyListener(new ModifyListener()
+		{
+			@Override
+			public void modifyText(final ModifyEvent e)
+			{
+				DomainEditor.this.setDirty(true);
+			}
+		});
+
+		label = this.formToolkit.createLabel(composite, "Strasse", SWT.NONE);
+		label.setLayoutData(new GridData());
+
+		this.address = this.formToolkit.createText(composite, "");
+		this.address.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+		this.address.addModifyListener(new ModifyListener()
+		{
+			@Override
+			public void modifyText(final ModifyEvent e)
+			{
+				DomainEditor.this.setDirty(true);
+			}
+		});
+
+		label = this.formToolkit.createLabel(composite, "Ort", SWT.NONE);
+		label.setLayoutData(new GridData());
+
+		this.city = this.formToolkit.createText(composite, "");
+		this.city.setLayoutData(new GridData(GridData.FILL_BOTH));
+		this.city.addModifyListener(new ModifyListener()
+		{
+			@Override
+			public void modifyText(final ModifyEvent e)
+			{
+				DomainEditor.this.setDirty(true);
+			}
+		});
+
+		label = this.formToolkit.createLabel(composite, "Telefon", SWT.NONE);
+		label.setLayoutData(new GridData());
+
+		this.phone = this.formToolkit.createText(composite, "");
+		this.phone.setLayoutData(new GridData(GridData.FILL_BOTH));
+		this.phone.addModifyListener(new ModifyListener()
+		{
+			@Override
+			public void modifyText(final ModifyEvent e)
+			{
+				DomainEditor.this.setDirty(true);
+			}
+		});
+
+		label = this.formToolkit.createLabel(composite, "Fax", SWT.NONE);
+		label.setLayoutData(new GridData());
+
+		this.fax = this.formToolkit.createText(composite, "");
+		this.fax.setLayoutData(new GridData(GridData.FILL_BOTH));
+		this.fax.addModifyListener(new ModifyListener()
+		{
+			@Override
+			public void modifyText(final ModifyEvent e)
+			{
+				DomainEditor.this.setDirty(true);
+			}
+		});
+
+		label = this.formToolkit.createLabel(composite, "Email", SWT.NONE);
+		label.setLayoutData(new GridData());
+
+		this.email = this.formToolkit.createText(composite, "");
+		this.email.setLayoutData(new GridData(GridData.FILL_BOTH));
+		this.email.addModifyListener(new ModifyListener()
+		{
+			@Override
+			public void modifyText(final ModifyEvent e)
+			{
+				DomainEditor.this.setDirty(true);
+			}
+		});
+
+		label = this.formToolkit.createLabel(composite, "Website", SWT.NONE);
+		label.setLayoutData(new GridData());
+
+		this.website = this.formToolkit.createText(composite, "");
+		this.website.setLayoutData(new GridData(GridData.FILL_BOTH));
+		this.website.addModifyListener(new ModifyListener()
+		{
+			@Override
+			public void modifyText(final ModifyEvent e)
+			{
+				DomainEditor.this.setDirty(true);
+			}
+		});
+
+		this.formToolkit.paintBordersFor(composite);
+
+		return composite;
+	}
+
+	private Message getEmptyNameMessage()
+	{
+		Message msg = null;
+
+		if (this.name.getText().isEmpty())
+		{
+			msg = new Message(this.name, "Fehler");
+			msg.setMessage("Die Domäne muss eine Bezeichnung haben.");
+		}
+
+		return msg;
+	}
+
 	@Override
-	protected Message getMessage(PersistenceException.ErrorCode errorCode)
+	protected Message getMessage(final PersistenceException.ErrorCode errorCode)
 	{
 		Message msg = null;
 
@@ -141,6 +306,23 @@ public class DomainEditor extends AbstractEntityEditor<Domain>
 			msg = this.getUniqueCodeMessage();
 		}
 		return msg;
+	}
+
+	@Override
+	protected String getName()
+	{
+		DomainEditorInput input = (DomainEditorInput) this.getEditorInput();
+		Domain domain = (Domain) input.getAdapter(Domain.class);
+		return domain.getId() == null ? "Neu" : (domain.getCode().length() == 0 ? "???" : domain.getCode());
+	}
+
+	@Override
+	protected String getText()
+	{
+		DomainEditorInput input = (DomainEditorInput) this.getEditorInput();
+		Domain domain = (Domain) input.getAdapter(Domain.class);
+		return domain.getId() == null ? "Neue Domäne" : "Domäne "
+				+ (domain.getCode().length() == 0 ? domain.getName() : domain.getCode());
 	}
 
 	private Message getUniqueCodeMessage()
@@ -170,34 +352,10 @@ public class DomainEditor extends AbstractEntityEditor<Domain>
 		return msg;
 	}
 
-	private Message getEmptyNameMessage()
-	{
-		Message msg = null;
-
-		if (this.name.getText().isEmpty())
-		{
-			msg = new Message(this.name, "Fehler");
-			msg.setMessage("Die Domäne muss eine Bezeichnung haben.");
-		}
-
-		return msg;
-	}
-
 	@Override
-	protected String getName()
+	protected void initialize()
 	{
-		DomainEditorInput input = (DomainEditorInput) this.getEditorInput();
-		Domain domain = (Domain) input.getAdapter(Domain.class);
-		return domain.getId() == null ? "Neu" : (domain.getCode().length() == 0 ? "???" : domain.getCode());
-	}
-
-	@Override
-	protected String getText()
-	{
-		DomainEditorInput input = (DomainEditorInput) this.getEditorInput();
-		Domain domain = (Domain) input.getAdapter(Domain.class);
-		return domain.getId() == null ? "Neue Domäne" : "Domäne "
-				+ (domain.getCode().length() == 0 ? domain.getName() : domain.getCode());
+		EntityMediator.addListener(Domain.class, this);
 	}
 
 	@Override
@@ -210,6 +368,13 @@ public class DomainEditor extends AbstractEntityEditor<Domain>
 			this.code.setText(domain.getCode());
 			this.name.setText(domain.getName());
 			this.description.setText(domain.getDescription());
+			this.organization.setText(domain.getOrganization());
+			this.address.setText(domain.getAddress());
+			this.city.setText(domain.getCity());
+			this.phone.setText(domain.getPhone());
+			this.fax.setText(domain.getFax());
+			this.email.setText(domain.getEmail());
+			this.website.setText(domain.getWebsite());
 		}
 		this.setDirty(false);
 	}
@@ -224,7 +389,20 @@ public class DomainEditor extends AbstractEntityEditor<Domain>
 			domain.setCode(this.code.getText());
 			domain.setName(this.name.getText());
 			domain.setDescription(this.description.getText());
+			domain.setOrganization(this.organization.getText());
+			domain.setAddress(this.address.getText());
+			domain.setCity(this.city.getText());
+			domain.setPhone(this.phone.getText());
+			domain.setFax(this.fax.getText());
+			domain.setEmail(this.email.getText());
+			domain.setWebsite(this.website.getText());
 		}
+	}
+
+	@Override
+	public void setFocus()
+	{
+		this.code.setFocus();
 	}
 
 	@Override
@@ -242,20 +420,8 @@ public class DomainEditor extends AbstractEntityEditor<Domain>
 	}
 
 	@Override
-	protected boolean validateType(AbstractEntityEditorInput<Domain> input)
+	protected boolean validateType(final AbstractEntityEditorInput<Domain> input)
 	{
 		return input.getAdapter(Domain.class) instanceof Domain;
-	}
-
-	@Override
-	public void setFocus()
-	{
-		this.code.setFocus();
-	}
-
-	@Override
-	public void dispose()
-	{
-		EntityMediator.removeListener(Domain.class, this);
 	}
 }
