f8c0f30c5bb14bcee344edb05b49c96594e3bd8d
Christian Eugster <christian.eugster@gmx.net>
2019-06-27 16:34:20 +0000

    Changed Sort Order in AddressGroupMemberSorter

--- ch.eugster.events.addressgroup/src/ch/eugster/events/addressgroup/views/AddressGroupMemberTreeSorter.java
+++ ch.eugster.events.addressgroup/src/ch/eugster/events/addressgroup/views/AddressGroupMemberTreeSorter.java
@@ -5,6 +5,7 @@ import org.eclipse.jface.viewers.ViewerSorter;
 
 import ch.eugster.events.addressgroup.AddressGroupMemberSelector;
 import ch.eugster.events.persistence.model.AddressGroup;
+import ch.eugster.events.persistence.model.AddressGroupCategory;
 
 public class AddressGroupMemberTreeSorter extends ViewerSorter
 {
@@ -17,18 +18,40 @@ public class AddressGroupMemberTreeSorter extends ViewerSorter
 	}
 
 	@Override
-	public int compare(Viewer viewer, Object element1, Object element2)
+	public int compare(Viewer viewer, Object e1, Object e2)
 	{
-		if (element1 instanceof AddressGroup && element2 instanceof AddressGroup)
+		if (e1 instanceof AddressGroupCategory)
 		{
-			AddressGroup addressGroup1 = (AddressGroup) element1;
-			AddressGroup addressGroup2 = (AddressGroup) element2;
+			if (e2 instanceof AddressGroupCategory)
+			{
+				AddressGroupCategory category1 = (AddressGroupCategory) e1;
+				AddressGroupCategory category2 = (AddressGroupCategory) e2;
 
-			boolean checked1 = selector.isChecked(addressGroup1);
-			boolean checked2 = selector.isChecked(addressGroup2);
+				if (category1.getCode().equals(category2.getCode()))
+					return category1.getName().compareTo(category2.getName());
+				else
+					return category1.getCode().compareTo(category2.getCode());
+			}
+		}
+		else if (e1 instanceof AddressGroup && e2 instanceof AddressGroup)
+		{
+			AddressGroup group1 = (AddressGroup) e1;
+			AddressGroup group2 = (AddressGroup) e2;
+
+			boolean checked1 = selector.isChecked(group1);
+			boolean checked2 = selector.isChecked(group2);
 
 			if (checked1 == checked2)
-				return addressGroup1.getCode().compareTo(addressGroup2.getCode());
+			{
+				if (group1.getCode().equals(group2.getCode()))
+				{
+					return group1.getName().compareTo(group2.getName());
+				}
+				else
+				{
+					return group1.getCode().compareTo(group2.getCode());
+				}
+			}
 			else if (checked1)
 				return -1;
 			else if (checked2)
